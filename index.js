e60f7d35442143d33c0c582363475e5d10652305
Victoria <victoria@ghostbear.local>
2021-11-04 15:24:30 +0100

    Added the s3 delete function

---
+++ index.js
@@ -0,0 +1,45 @@
+const { DeleteObjectCommand } = require("@aws-sdk/client-s3");
+const { s3Client } = require("./sampleClient.js"); // Helper function that creates Amazon S3 service client module.
+
+const orphans = require("./generated-csv/s3_orphan_urls.json");
+
+const orphan_urls = orphans.map((o) => o["Formelo CDN URL"]);
+
+console.log("orphan_urls", orphan_urls);
+
+let s3Keys = [];
+
+const getS3Keys = () => {
+  for (let i = 0; i < orphan_urls.length; i++) {
+    if (orphan_urls[i].includes("cdn.formelo.com")) {
+      s3Keys.push(orphan_urls[i].split("cdn.formelo.com")[1].substring(1));
+    }
+  }
+};
+
+const run = async (s3Key) => {
+  try {
+    const data = await s3Client.send(
+      new DeleteObjectCommand({
+        Bucket: "amplify-myfirstapp-dev-222051-deployment",
+        Key: s3Key,
+      })
+    );
+    console.log(`${s3Key} has been deleted successfully.`);
+    return data; // For unit tests.
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+const deletedS3OrphanUrls = () => {
+  getS3Keys();
+  //console.log("s3Keys", s3Keys);
+
+  for (let i = 0; i < s3Keys.length; i++) {
+    //  console.log(i, "s3Key: ", s3Keys[i]);
+    run(s3Keys[i]);
+  }
+};
+
+deletedS3OrphanUrls();
